ec116484aff8e3c9622d76e80e6a4462723cd60a
Valentine <valyuscha2004@gmail.com>
2020-06-27 12:57:01 +0300

    Meke todo app

---
+++ build/precache-manifest.c31e302776fa30fcb822d82c6e7836f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a43722b543579ee9814cd5daab7e965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e450ab122d8167f2c85f",
+    "url": "/static/js/2.bd195b40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd195b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0937cdc1a72c1e97ae",
+    "url": "/static/js/main.de12aab3.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
